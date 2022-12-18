4a34d98fcba97c38b49d36f473aac42360a118b7
Jorge Panzera <panzera.jorge@gmail.com>
2022-12-18 19:38:34 -0300

    Ejercicios 3 y 4

--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -3,12 +3,18 @@ import "./aboutme.js";
 console.log("Ejercicio 1");
 
 // Ejercicio 2
-console.log("Ver pdf adjunto");
+console.log("Ejercicio 2 - Ver pdf adjunto");
 
 // Ejercicio 3
-console.log("Ver pdf adjunto");
+console.log("Ejercicio 3 - Ver pdf adjunto");
 
 // Ejercicio 4
+console.log("Ejercicio 4 - Divisibles por 7 entre 1 y 100 ");
+for (let i=1; i<=100; i++) {
+    if ((i%7)==0) {
+        console.log(`${i} es divisible entre 7`);
+    }
+}
 
 // Ejercicio 5
 
