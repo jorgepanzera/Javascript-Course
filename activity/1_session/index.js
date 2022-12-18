8df5237b0c6485ce0e4cf8dec429ba4f789e67bb
Jorge Panzera <panzera.jorge@gmail.com>
2022-12-18 20:59:47 -0300

    Prototipo Ejercicio 6

--- activity/1_session/index.js
+++ activity/1_session/index.js
@@ -21,4 +21,58 @@ console.log("Ejercicio 5 - Ver pdf adjunto");
 
 
 // Ejercicio 6
+console.log("Ejercicio 6 - Objeto con métodos");
 
+// constructor function
+function Formatter () {
+    this.prefix = "Hello",
+    this.append = function(cadenaAppend) {
+        console.log(this.prefix + " " + cadenaAppend);
+    }
+}
+
+// create object
+let  formatter = new Formatter();
+
+// metodo append
+formatter.append("para todos los colegas");
+
+// agregar metodo con prototype
+Formatter.prototype.toLowerString = function(cadenaLower) {
+    cadenaLower.toString();
+    console.log(cadenaLower.toLowerCase());
+    }
+
+let formatter2 = new Formatter();
+
+formatter2.toLowerString("CONVERTIR todo A MINUSculas");
+
+/*
+let formatterBase = {
+    prefix: "Hello",
+    append: function(cadenaAppend) {
+        console.log(formatterBase.prefix + " " + cadenaAppend);
+    }
+}
+
+
+let formatter = Object.create(formatterBase);
+
+formatter.append("para todos los colegas");
+
+// agregar metodo usando prototype
+formatter.prototype.toLowerString = function (x) {
+    console.log(x.toLowerCase);
+    } 
+
+formatter.toLowerString("CONVERTIR a minucUSLAS");
+
+
+car.stop = function() {
+    console.log("Stop the engine...");
+}
+
+console.log(vehicle);
+console.log(car);
+
+*/
