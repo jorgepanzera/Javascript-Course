711b0c4b82ba4806def9152e45693923621081a6
Jorge <panzera.jorge@gmail.com>
2022-12-31 13:30:16 -0300

    Actividad 3 Ejercicio 1

--- activity/typescript/index.js
+++ activity/typescript/index.js
@@ -1,4 +1,21 @@
-// Ejercicio 1
+var PersonToArrayv1 = function (person) {
+    var aResult;
+    aResult = [];
+    aResult.push(person.name);
+    aResult.push(person.age);
+    aResult.push(person.profession);
+    return aResult;
+    //return Object.values(person);
+};
+// version optimizada utilizando metodo Object.values
+var PersonToArray = function (person) {
+    // return Object.values(person); .values no andaba con el compilador
+    return Object.keys(person).map(function (key) { return person[key]; });
+};
+var aPerson = { name: "Jorge", age: 50, profession: "Ingeniero" };
+// ambas versiones dan el mismo resultado
+console.log(PersonToArrayv1(aPerson));
+console.log(PersonToArray(aPerson));
 // Ejercicio 2
 // Ejercicio 3
 // Ejercicio 4
--- activity/typescript/index.ts
+++ activity/typescript/index.ts
@@ -1,4 +1,33 @@
 // Ejercicio 1
+interface Person {
+    name: string;
+    age: number;
+    profession: string;
+}
+
+type aNumberOrString = Array<number | string> 
+
+let PersonToArrayv1 = (person: Person): aNumberOrString => {
+    let aResult : aNumberOrString
+    aResult = []
+    aResult.push(person.name)
+    aResult.push(person.age)
+    aResult.push(person.profession)
+    return aResult
+    //return Object.values(person);
+}
+
+// version optimizada utilizando metodo Object.keys
+let PersonToArray = (person: Person): aNumberOrString => {
+    // return Object.values(person); .values no andaba con el compilador (tema de version()
+    return Object.keys(person).map(key => person[key]);
+}
+
+let aPerson : Person = { name:"Jorge", age: 50, profession: "Ingeniero"}
+
+// ambas versiones dan el mismo resultado
+console.log(PersonToArrayv1(aPerson))
+console.log(PersonToArray(aPerson))
 
 // Ejercicio 2
 
