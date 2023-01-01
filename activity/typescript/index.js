7ae1c9748a260fe995e46299ecf00e0d2745bda0
Jorge <panzera.jorge@gmail.com>
2023-01-01 20:51:11 -0300

    Actividad 3 Ejercicio 2

--- activity/typescript/index.js
+++ activity/typescript/index.js
@@ -16,7 +16,23 @@ var aPerson = { name: "Jorge", age: 50, profession: "Ingeniero" };
 // ambas versiones dan el mismo resultado
 console.log(PersonToArrayv1(aPerson));
 console.log(PersonToArray(aPerson));
-// Ejercicio 2
+function sumOrConcatenate(x, y) {
+    if (typeof x === "number" && typeof y === "number") {
+        return x + y;
+    }
+    if (typeof x === "string" && typeof y === "string") {
+        return x + "-" + y;
+    }
+    if (typeof x === "string" && typeof y === "number") {
+        return x + "+" + String(y);
+    }
+    if (typeof x === "number" && typeof y === "string") {
+        return String(x) + "+" + y;
+    }
+    throw new Error("Expected string or number in both parameters, x is '".concat(typeof x, "', y is ").concat(typeof y));
+}
+console.log(sumOrConcatenate(4, 3)); // 7
+console.log(sumOrConcatenate(4, "hello")); // 4+hello 
+console.log(sumOrConcatenate("hello", "world")); //hello-world
 // Ejercicio 3
 // Ejercicio 4
-//# sourceMappingURL=index.js.map
--- activity/typescript/index.ts
+++ activity/typescript/index.ts
@@ -31,6 +31,29 @@ console.log(PersonToArray(aPerson))
 
 // Ejercicio 2
 
+type NumberOrString = number | string
+
+function sumOrConcatenate(x: NumberOrString, y: NumberOrString) : NumberOrString {
+    if (typeof x === "number" && typeof y === "number") {
+        return x + y
+    }
+    if (typeof x === "string" && typeof y === "string"){
+        return x + "-" + y
+    }
+    if (typeof x === "string" && typeof y === "number"){
+        return x + "+" + String(y)
+    }
+    if (typeof x === "number" && typeof y === "string"){
+        return String(x) + "+" + y
+    }
+    throw new Error(`Expected string or number in both parameters, x is '${typeof x}', y is ${typeof y}`);
+}
+
+console.log(sumOrConcatenate(4, 3)); // 7
+console.log(sumOrConcatenate(4, "hello")); // 4+hello 
+console.log(sumOrConcatenate("hello", "world")); //hello-world
+
+
 // Ejercicio 3
 
 // Ejercicio 4
